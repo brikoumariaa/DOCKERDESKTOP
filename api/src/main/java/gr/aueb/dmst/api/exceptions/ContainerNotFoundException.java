0195af20d992130c2f76376e199e81e59902aa99
MiguelM729 <150272385+MiguelM729@users.noreply.github.com>
2024-01-16 20:52:31 +0200

    Update ContainerNotFoundException.java

--- api/src/main/java/gr/aueb/dmst/api/exceptions/ContainerNotFoundException.java
+++ api/src/main/java/gr/aueb/dmst/api/exceptions/ContainerNotFoundException.java
@@ -1,5 +1,7 @@
 package gr.aueb.dmst.api.exceptions;
 
+// Exception that occurs when the given id does not match that of a Container
+
 public class ContainerNotFoundException extends RuntimeException {
     public ContainerNotFoundException(String id) {
         super("No such container: " + id);
