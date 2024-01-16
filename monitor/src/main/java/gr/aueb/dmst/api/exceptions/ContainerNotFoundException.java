e31842d9707a0b27b8eb0488d0cd623ba6b97cd3
brikoumariaa <t8220102@aueb.gr>
2024-01-16 14:23:49 +0200

    deleted:    monitor/src/main/java/gr/aueb/dmst/api/Application.java         deleted:    monitor/src/main/java/gr/aueb/dmst/api/config/DockerClientConfiguration.java         deleted:    monitor/src/main/java/gr/aueb/dmst/api/controllers/ContainerController.java         deleted:    monitor/src/main/java/gr/aueb/dmst/api/entities/ContainerInstances.java         deleted:    monitor/src/main/java/gr/aueb/dmst/api/entities/ContainerMetrics.java         deleted:    monitor/src/main/java/gr/aueb/dmst/api/exceptions/ContainerNotFoundAdvice.java         deleted:    monitor/src/main/java/gr/aueb/dmst/api/exceptions/ContainerNotFoundException.java         deleted:    monitor/src/main/java/gr/aueb/dmst/api/repositories/ContainerInstancesRepository.java         deleted:    monitor/src/main/java/gr/aueb/dmst/api/repositories/ContainerMetricsRepository.java

--- monitor/src/main/java/gr/aueb/dmst/api/exceptions/ContainerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package gr.aueb.dmst.api.exceptions;
-
-public class ContainerNotFoundException extends RuntimeException {
-    public ContainerNotFoundException(String id) {
-        super("No such container: " + id);
-    }
-}
