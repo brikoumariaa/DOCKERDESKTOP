477f8a084b83a2b107cb7cc9b1854ce0d79ad15b
brikoumariaa <t8220102@aueb.gr>
2024-01-16 14:00:01 +0200

    removes

--- api/src/main/java/gr/aueb/dmst/api/repositories/ContainerInstancesRepository.java
+++
@@ -1,9 +0,0 @@
-package gr.aueb.dmst.api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import gr.aueb.dmst.api.entities.ContainerInstances;
-
-public interface ContainerInstancesRepository extends JpaRepository<ContainerInstances, String> {
-    
-}
