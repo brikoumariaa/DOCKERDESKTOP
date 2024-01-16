c583ffb680ac1d588a17b6939a2667ec032679ed
MiguelM729 <150272385+MiguelM729@users.noreply.github.com>
2024-01-16 20:53:20 +0200

    Update ContainerInstancesRepository.java

--- api/src/main/java/gr/aueb/dmst/api/repositories/ContainerInstancesRepository.java
+++ api/src/main/java/gr/aueb/dmst/api/repositories/ContainerInstancesRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import gr.aueb.dmst.api.entities.ContainerInstances;
 
+// A repository for every ContainerInstance
+
 public interface ContainerInstancesRepository extends JpaRepository<ContainerInstances, String> {
     
 }
