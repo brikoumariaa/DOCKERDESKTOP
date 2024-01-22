838d44de1f495b60ea3c0aff7f2582ea8c73f824
brikoumariaa <147167872+brikoumariaa@users.noreply.github.com>
2024-01-22 23:37:29 +0200

    Update ContainerMetrics.java

--- api/src/main/java/gr/aueb/dmst/api/entities/ContainerMetrics.java
+++ api/src/main/java/gr/aueb/dmst/api/entities/ContainerMetrics.java
@@ -1,7 +1,8 @@
+// Package declaration for the 'entities' package
 package gr.aueb.dmst.api.entities;
 
+// Importing necessary classes from the Java Persistence API
 import java.sql.Timestamp;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,27 +11,34 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+// Entity annotation indicates that this class is a JPA entity
 @Entity
 public class ContainerMetrics {
+    // Primary key for the entity, auto-generated using Identity strategy
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Column annotation for mapping 'measurementTimestamp' to a database column
     @Column(name = "measurement_timestamp", nullable = false)
     private Timestamp measurementTimestamp;
 
+    // Column annotation for mapping 'cpuPercent' to a database column
     @Column(name = "cpu_percent", nullable = false)
     private Double cpuPercent;
 
+    // Column annotation for mapping 'memoryPercent' to a database column
     @Column(name = "memory_percent", nullable = false)
     private Double memoryPercent;
-
+    
+    // Many-to-One association with ContainerInstances, indicating a foreign key relationship
     @ManyToOne
     @JoinColumn(name = "container_instance_id", nullable = false, referencedColumnName = "id")
     private ContainerInstances containerInstances;
 
     public ContainerMetrics() {}
 
+    // Parameterized constructor for creating instances with initial values
     public ContainerMetrics(
         Long id,
         Timestamp measurementTimestamp,
